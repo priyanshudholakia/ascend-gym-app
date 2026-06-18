9a14936bb8fff1c3ce12add5b7dcde2abd2dbdf0
Priyanshu Dholakia <priyanshudholakia1311@gmail.com>
2026-06-19 03:32:54 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://ascend-gym-app.vercel.app'],
+  credentials: true
+}))
 app.use(express.json());
 
 // Routes
